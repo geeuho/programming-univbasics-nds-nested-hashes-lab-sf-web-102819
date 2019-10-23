d71d03a29d0162bfc6bcdb8711b6b5df66ac95f8
Allen Shin <geeuho@gmail.com>
2019-10-23 00:35:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,10 +11,10 @@ def assembled_aoh
 end
 
 def literal_aoh
-  [{ name:  "Don Gately", occupation: "Live-in Staff" }
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-  { name:  "Pat Monteseian", occupation: "Staff" }
-  { name:  "Kate Gompert", occupation: "None" }
+  [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
   { name:  "Bruce Green", occupation: "Fan of Mildred" }];
 end
 
